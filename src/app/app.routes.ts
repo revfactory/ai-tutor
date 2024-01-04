b7662f721564358c57e054c7fd3a3118bc28f36f
robin-hwang <robin.hwang@kakaocorp.com>
2024-01-04 21:21:32 +0900

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TutorListComponent } from './tutor-list/tutor-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tutors',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tutors',
+    component: TutorListComponent
+  },
+];
---
+++ src/app/tutor-list/tutor-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutor-list.component.html',
+  styleUrl: './tutor-list.component.scss'
+})
+export class TutorListComponent {
+
+}
